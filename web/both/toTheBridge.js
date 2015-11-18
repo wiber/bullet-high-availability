0210c4d9d681606009c6859442e66bab1b7618c8
wiber <elias@youiest.com>
2015-11-18 19:04:22 +0000

    server true latency comp

--- web/both/toTheBridge.js
+++ web/both/toTheBridge.js
@@ -2,25 +2,50 @@ App = {}
 App.connect = function ( from, to, object ) {
   console.log( args )
 }
-App.step = function ( stepTo ) {
-  console.log( stepTo )
+App.step = function ( to ) {
+  console.log( to )
     // on landing somewhere, call this, (when called on server? not inconceivable..)
     // add location to your journey array on user object
     // set stateful field so that when text enters box it's known what connection it relates to
     // function calls a methodcall that manages collection data on client and server
     // with latency compensation
-  callback = function ( error, result ) {
+
+  Meteor.call( "stepTo", to, function ( error, result ) {
     if ( error ) {
       console.log( "error", error );
     }
     if ( result ) {
       console.log( "result", result );
     }
-  }
-  Meteor.call( "meteorMethod", dataObject, callback( error, result ) );
-}
+  } );
 
+}
+Meteor.methods( {
+  stepTo: function ( to ) {
+    if ( Meteor.isServer ) {
+      console.log( to + ' server ' + Meteor.isServer )
+    } else {
+      console.log( to + ' server ' + Meteor.isServer )
+    }
+    return 42;
+  }
+} );
 pub = function () {
+  Meteor.publish( "userData", function () {
+    if ( this.userId ) {
+      return Meteor.users.find( {
+        _id: this.userId
+      }, {
+        fields: {
+          'steps': 1,
+          'connects': 1
+        }
+      } );
+    } else {
+      this.ready();
+    }
+  } );
+
   steps = new Mongo.Collection( "steps" );
   steps.allow( {
     insert: function () {
@@ -44,6 +69,7 @@ pub = function () {
 
 sub = function () {
   Meteor.subscribe( "userObjs", this.userId );
+  Meteor.subscribe( "userData" );
 }
 
 
@@ -57,10 +83,9 @@ truncateLogs = function () {
     var fs = Npm.require( 'fs' )
     var appRoot = process.env.PWD;
     webl = fs.truncate( appRoot + '/app.log', 0, function () {
-      console.log( ' done overwriting app.log in '+ appRoot )
+      console.log( ' done overwriting the app.log in ' + appRoot )
     } )
   } );
-
   // send SIGHUP to process
   grep.kill( 'SIGHUP' );
 }
--- web/client/search.js
+++ web/client/search.js
@@ -2,44 +2,54 @@ var options = {
   keepHistory: 1000 * 60 * 5,
   localSearch: true
 };
-var fields = ['packageName', 'description'];
+var fields = [ 'packageName', 'description' ];
 
-PackageSearch = new SearchSource('packages', fields, options);
-PackageSearch.fetchData = function(searchText, options, success) {
-  SearchConn.call('getPackages', searchText, options, function(err, data) {
-    success(err, data);
-  });
+PackageSearch = new SearchSource( 'packages', fields, options );
+PackageSearch.fetchData = function ( searchText, options, success ) {
+  SearchConn.call( 'getPackages', searchText, options, function ( err, data ) {
+    success( err, data );
+  } );
 };
 
-Template.searchResult.helpers({
-  getPackages: function() {
-    return PackageSearch.getData({
-      transform: function(matchText, regExp) {
-        return matchText.replace(regExp, "<b>$&</b>")
+Template.searchResult.helpers( {
+  getPackages: function () {
+    return PackageSearch.getData( {
+      transform: function ( matchText, regExp ) {
+        return matchText.replace( regExp, "<b>$&</b>" )
       },
-      sort: {isoScore: -1}
-    });
+      sort: {
+        isoScore: -1
+      }
+    } );
   },
 
-  isLoading: function() {
+  isLoading: function () {
     return PackageSearch.getStatus().loading;
   }
-});
+} );
+Template.searchResult.events( {
+  "click a.to": function ( e, template ) {
+    console.log( e )
+    App.step( e.target.href )
+    return false;
+  }
+} );
 
-Template.searchBox.events({
-  "keyup #search-box": _.throttle(function(e) {
-    var searchBox = $('#search-result');
-    var text = $(e.target).val().trim();
-    if(text.length > 0) {
+Template.searchBox.events( {
+
+  "keyup #search-box": _.throttle( function ( e ) {
+    var searchBox = $( '#search-result' );
+    var text = $( e.target ).val().trim();
+    if ( text.length > 0 ) {
       searchBox.show();
     } else {
       searchBox.hide();
     }
 
-    PackageSearch.search(text);
-  }, 200)
-});
+    PackageSearch.search( text );
+  }, 200 )
+} );
 
-Template.searchBox.rendered = function() {
-  $('#search-result').hide();
+Template.searchBox.rendered = function () {
+  $( '#search-result' ).hide();
 };
